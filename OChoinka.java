f8fa7166efd65f962ecad122d99e5035154bef1a
FotzsyzrK123 <k.koska94@gmail.com>
2019-06-05 22:52:16 +0200

    first commit

---
+++ OChoinka.java
@@ -0,0 +1,14 @@
+public class ochoinka {
+	public static void main(String[] args) {
+	String x = "Krzys";
+	System.out.println(x);
+	System.out.println("x");
+
+	int wykrzykniki = 0;
+	while (wykrzykniki < 3) {
+		wykrzykniki = wykrzykniki + 1;
+	x = x + "!" ;
+	System.out.println(x);
+	}
+	}
+}
